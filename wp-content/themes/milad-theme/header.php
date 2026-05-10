fad923313c8c7038874900253fb7b07a82d7c139
milad kavianfar <miladkavianfar@milads-MacBook-Pro.local>
2026-05-11 00:12:11 +0200

    Setup basic WordPress theme structure and navbar

--- wp-content/themes/milad-theme/header.php
+++ wp-content/themes/milad-theme/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
   <title>Milad theme</title>
   <?php wp_head(); ?>
 </head>
@@ -9,3 +11,20 @@
 <header>
   <h1>My WordPress Theme</h1>
 </header>
+<nav>
+  <div class="navbar">
+    <div class="pic">
+      <img src="https://wallpapers.com/images/featured-full/office-desk-a1yivbaxal92jim2.jpg" alt="office desk" class="image">
+    </div>
+
+    <ul class="menu">
+      <li class="menu-item"><a href="#" class="menu-item-tag">home</a></li>
+      <li class="menu-item"><a href="#" class="menu-item-tag">about</a></li>
+      <li class="menu-item"><a href="#" class="menu-item-tag">contact</a></li>
+    </ul>
+
+    <div class="nav-action">
+      <button>sign in</button>
+    </div>
+  </div>
+</nav>
